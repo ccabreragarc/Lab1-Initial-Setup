857fbaae0ef2b5b499ae61caed79ef92460f921d
Cesar <ccabreragarc2023@fau.edu>
2024-01-25 09:36:05 -0500

    initial commit

---
+++ Lab1/timetabled/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+        <h1> Itinerary for 7 Days in Chicago</h1>
+        <h2>Welcome to Chicago, Cesar! Check out this calendar to get to know the city and see all the sights during your stay.</h2>
+      
+    </div>
+  )
+}
+
+export default App
